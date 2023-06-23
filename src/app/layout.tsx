3ed1ac20dc6f09cec941185403cd93854f270538
BlazWeb <moxy.blaz@gmail.com>
2023-06-23 19:26:16 +0200

    Add existing project files to Git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import "./globals.css";
+import Navbar from "./dashboard/components/Navbar";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import { ConfirmationProvider } from "./context/ConfirmationContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>{children}</SessionProvider>
+        <SessionProvider session={session}>
+          <ConfirmationProvider>
+            <Navbar />
+            {children}
+          </ConfirmationProvider>
+        </SessionProvider>
       </body>
     </html>
   );
